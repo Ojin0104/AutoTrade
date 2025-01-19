8ebb3f4b471ee3a5f6119a98b9d0e950925eb17e
yeongjin <hanyj0104@naver.com>
2025-01-19 22:57:32 +0900

    test: add api test for getUpbitAccount()

--- src/test/java/yj/AutoTrade/upbit/UpbitApiClientTest.java
+++ src/test/java/yj/AutoTrade/upbit/UpbitApiClientTest.java
@@ -4,14 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import yj.AutoTrade.upbit.dto.UpbitAccountResponseDto;
 import yj.AutoTrade.upbit.dto.UpbitTickerResponseDto;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @TestPropertySource(properties = {
         "upbit.url=https://api.upbit.com",
-        "upbit.access-key=dummy-access-key",
-        "upbit.secret-key=dummy-secret-key"
+        "upbit.access-key=access",
+        "upbit.secret-key=secret"
 })
 class UpbitApiClientTest {
     @Autowired
@@ -29,5 +30,15 @@ class UpbitApiClientTest {
         assertNotNull(response);
         //assertTrue(response.contains("KRW-BTC"));
         System.out.println("Upbit API Response: " + response[0]);
+    }
+
+    @Test
+    void testGetUpbitAccount() {
+
+        UpbitAccountResponseDto[] upbitAccount = upbitApiClient.getUpbitAccount();
+
+        assertNotNull(upbitAccount);
+
+        System.out.println("Upbit API Response: " + upbitAccount[0]);
     }
 }
