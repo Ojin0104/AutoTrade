9f5e52115a9843093c76d82325e4c24de6371de8
yeongjin <hanyj0104@naver.com>
2025-06-13 10:31:26 +0900

    feat: 공통 Response 추가

--- src/main/java/yj/AutoTrade/exception/GlobalExceptionHandler.java
+++ src/main/java/yj/AutoTrade/exception/GlobalExceptionHandler.java
@@ -1,26 +1,19 @@
 package yj.AutoTrade.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import yj.AutoTrade.common.ApiResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TradeException.class)
-    public ResponseEntity<ErrorResponse> handleTradeException(TradeException ex) {
+    public ResponseEntity<ApiResponse> handleTradeException(TradeException ex) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(ex.getErrorCode(), ex.getErrorMessage()));
+                .body(new ApiResponse(ex.getErrorCode(), ex.getErrorMessage()));
     }
 
-    @Getter
-    @AllArgsConstructor
-    public static class ErrorResponse {
-        private String code;
-        private String message;
-    }
 }
